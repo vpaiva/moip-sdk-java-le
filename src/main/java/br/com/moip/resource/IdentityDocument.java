a495d65ab4d5d1d0b62c6c96a2f3500daa6a878c
Vinício Paiva <vpaiva@gmail.com>
2018-10-29 16:41:39 -0300

    Aumentar visibilidade para que o valor consiga ser adquirido.

--- src/main/java/br/com/moip/resource/IdentityDocument.java
+++ src/main/java/br/com/moip/resource/IdentityDocument.java
@@ -1,37 +1,37 @@
 package br.com.moip.resource;
 
 public class IdentityDocument {
-
+    
     private Type type;
     private String number;
     private String issuer;
     private ApiDate issueDate;
-
+    
     public String getNumber() {
-        return number;
+        return this.number;
     }
-
+    
     public String getIssuer() {
-        return issuer;
+        return this.issuer;
     }
-
+    
     public ApiDate getIssueDate() {
-        return issueDate;
+        return this.issueDate;
     }
-
+    
     public Type getType() {
-        return type;
+        return this.type;
     }
-
-    private enum Type {
+    
+    public enum Type {
         RG
     }
-
+    
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("IdentityDocument{");
-        sb.append("type='").append(type).append('\'');
-        sb.append(", number='").append(number).append('\'');
+        sb.append("type='").append(this.type).append('\'');
+        sb.append(", number='").append(this.number).append('\'');
         sb.append("}");
         return sb.toString();
     }
